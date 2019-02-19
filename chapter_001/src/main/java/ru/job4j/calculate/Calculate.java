24d21f1a90ac5e18a98a265836227b834fd40f2a
AlexTsoy <alextsoy1984@mail.ru>
2019-02-20 00:23:48 +0600

    1.3. JavaDoc. Code convention[#114818]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
-package  ru.job4j.calculate;
+﻿package  ru.job4j.calculate;
+
+/**
+ * Calculate.
+ *
+ * @author Alexey Tsoy
+ */
 
 public class Calculate {
+
+	/**
+	 * Конструктор, вывода строки в консоль.
+	 * @param arg -arg.
+	 */
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexey Tsoy (alextsoy1984@mail.ru)
+* @version 1
+* @since 20.02.2019
+*/
+package ru.job4j.calculate;
