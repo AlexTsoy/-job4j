75d07d01000d9d1165de6050b433a6266d7fc118
AlexTsoy <alextsoy1984@mail.ru>
2019-03-07 03:36:22 +0600

    6.1. Классический поиск перебором.[#114796] Дописать тест

--- chapter_001/src/test/java/ru/job4j/Array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/Array/FindLoopTest.java
@@ -10,22 +10,38 @@ import static org.junit.Assert.assertThat;
  * Test.
  *
  * @author Alexey Tsoy (alextsoy1984@mail.ru)
- * @version 1
+ * @version 2
  * @since 07.03.2019
  */
 public class FindLoopTest {
+
     /**
      * Метод для тестирования, поиск индекса элемента массива
      * @param value условие для поиска элемента массива
      * expect предпологаемый результат
      */
     @Test
-    public void whenArrayHas5Then0(){
+    public void whenArrayHas40Then3(){
         FindLoop find = new FindLoop();
         int[] input = new int[] {1, 10, 5, 40};
         int value = 40;
         int result = find.indexOf(input, value);
         int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Метод для тестирования, поиск не существующего индекса элемента массива
+     * @param value условие для поиска элемента массива
+     * expect предпологаемый результат
+     */
+    @Test
+    public void whenArrayHasNoElement(){
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {1, 10, 5, 40};
+        int value = 9;
+        int result = find.indexOf(input, value);
+        int expect = -1;
         assertThat(result, is(expect));
     }
 }
