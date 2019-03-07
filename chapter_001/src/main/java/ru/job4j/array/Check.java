5ff15621329597f51808534b4eafabb5313277ab
AlexTsoy <alextsoy1984@mail.ru>
2019-03-08 04:53:36 +0600

    6.3. Массив заполнен true или false[#114799]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ * class Check.
+ *
+ * @author Alexey Tsoy (alextsoy1984@mail.ru)
+ * @version 1
+ * @since 08.03.2019
+ */
+public class Check {
+
+    /**
+     * Метод проверки массива на boolean значения true или false.
+     * @param data необходимый нам для проверки массив.
+     * return результат
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = false;
+        for (int index = 0; index != data.length - 1; index++) {
+            if (data[0] != data[index + 1]){
+                result = false;
+                break;
+            } else result = true;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/Array/CheckTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.Array;
+
+import org.junit.Test;
+import ru.job4j.array.Check;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Alexey Tsoy (alextsoy1984@mail.ru)
+ * @version 1
+ * @since 08.03.2019
+ */
+public class CheckTest {
+
+    /**
+     * Метод для тестирования, проверить массива на boolean значения true или false
+     * @param input массив который нужно проверить
+     * is(value) предпологаемый результат
+     */
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Метод для тестирования, проверить массива на boolean значения true или false
+     * @param input массив который нужно проверить
+     * is(value) предпологаемый результат
+     */
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+
+}
